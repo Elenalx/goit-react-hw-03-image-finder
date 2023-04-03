845876ee226afdc20c7ab9f35dd51468fb8ccc97
Elena Blagodir <elenalx28@gmail.com>
2023-04-03 22:42:36 +0300

    add5

--- src/services/gallery-api.js
+++ src/services/gallery-api.js
@@ -1,10 +1,11 @@
+import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api'
 const API_KEY = '10499035-4c19632db287de98b060ef18d'
 
 
-export const galleryApi = (searchText, page) => {
-  return fetch(
-    `${BASE_URL}?q=${searchText}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+export const galleryApi = async (searchText, page) => {
+  const response = await axios.get(
+    `${BASE_URL}/api/?q=${searchText}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
   );
-   
+  return response.data;
 };
