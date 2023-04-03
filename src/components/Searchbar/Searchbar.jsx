fc499788d17da7d404b52f0bfb5e599fe7f107a1
Elena Blagodir <elenalx28@gmail.com>
2023-04-03 13:29:03 +0300

    add3

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './search-bar.module.css';
+
+class Searchbar extends Component {
+   state = {
+    value: '',
+  };
+
+  handleChange = e => {
+    this.setState({ value: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleSearch(this.state.value);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ value: '' });
+  };
+
+    render() {
+        const { value } = this.state;
+        
+        return (
+            <header className={css.searchBar}>
+                <form className={css.searchForm} onSubmit={this.handleSubmit}>
+                    <button type="submit" className={css.button}>
+                        <span class="button-label">Search</span>
+                    </button>
+
+                    <input
+                        name="searchText"
+                        className={css.input}
+                        type="text"
+                        value={value}
+                        autocomplete="off"
+                        autofocus
+                        placeholder="Search images and photos"
+                        onChange={this.handleChange}
+                    />
+                </form>
+            </header>
+            
+        );
+    }
+
+} 
+
+export default Searchbar;
+
+Searchbar.propTypes = {
+    handleSearch:PropTypes.func.isRequired,
+}
